7deae4c8ed09b3fef632233c8125fc1187f88de9
yathuchandran <yathuchandran67437@gmail.com>
2023-07-15 20:52:59 +0530

    post upload view post done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './Pages/Signup'
 import Login from "./Pages/Login";
 import Create from './Pages/Create'
+import view from './Pages/ViewPost'
 import {AuthContext, FirebaseContext} from './store/Context'
+import Post from "./store/PostContext";
 
 /**
  * ?  =====Import Components=====
@@ -22,14 +24,20 @@ function App() {
   })
   return (
     <div>
+      <Post>
+
       <BrowserRouter>
         <Routes>
           <Route Component={Home} path='/'></Route>
           <Route Component={Signup} path='/signup'></Route>
           <Route Component={Login} path='/login'></Route>
           <Route Component={Create} path='/create'></Route>
+          <Route Component={view} path='/view'></Route>
+
         </Routes>
       </BrowserRouter>
+      </Post>
+      
     </div>
   );
 }
